7f8c273430b9dab563b887d9b4c6ab358950d27d
lubomyr <lubomyr31@gmail.com>
2017-10-03 21:59:30 +0300

    implemented request READ_EXTERNAL_STORAGE permission

--- app/src/main/java/floaterr/floater/MainActivity.java
+++ app/src/main/java/floaterr/floater/MainActivity.java
@@ -1,11 +1,16 @@
 package floaterr.floater;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 import static floaterr.floater.NameKeys.KEY_IMAGE;
 import static floaterr.floater.NameKeys.KEY_VIDEO;
@@ -13,7 +18,8 @@ import static floaterr.floater.NameKeys.KEY_VIDEO;
 public class MainActivity extends Activity {
     private Uri imageUri;
     private Uri videoUri;
-    private final int REQUEST_OVERLAY_PERMISSION = 1;
+    private final int REQUEST_EXTERNAL_STORAGE = 1;
+    private final int REQUEST_OVERLAY_PERMISSION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +53,39 @@ public class MainActivity extends Activity {
         finish();
     }
 
+    public void verifyStoragePermissions() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            String[] PERMISSIONS_STORAGE = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+            return;
+        }
+        runService();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE:
+                if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    runService();
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
     private void checkDrawOverlayPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!Settings.canDrawOverlays(this)) {
@@ -54,10 +93,10 @@ public class MainActivity extends Activity {
                         Uri.parse("package:" + getPackageName()));
                 startActivityForResult(intent, REQUEST_OVERLAY_PERMISSION);
             } else {
-                runService();
+                verifyStoragePermissions();
             }
         } else {
-            runService();
+            verifyStoragePermissions();
         }
     }
 
@@ -66,7 +105,7 @@ public class MainActivity extends Activity {
         if (requestCode == REQUEST_OVERLAY_PERMISSION) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (Settings.canDrawOverlays(this)) {
-                    runService();
+                    verifyStoragePermissions();
                 }
             }
         }
